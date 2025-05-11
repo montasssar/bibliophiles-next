356b12d8ff248f2b7cade4da8adc1e3ab0ff0e76
mnt669 <montassar579@gmail.com>
2025-05-11 02:00:28 +0200

    allowed books.google.com for next/image

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['books.google.com'], // ✅ allow this domain
+    },
+  };
+  
+  module.exports = nextConfig;
+  
